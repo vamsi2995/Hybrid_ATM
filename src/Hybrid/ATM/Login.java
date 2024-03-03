87011c762d07c1a3c9ff279ea66b7e569f232e5a
vamsi2995 <reddyvamsi39@gmail.com>
2024-03-03 11:20:58 +0530

    comments added

--- src/Hybrid/ATM/Login.java
+++ src/Hybrid/ATM/Login.java
@@ -13,49 +13,49 @@ public class Login extends JFrame implements ActionListener {
     JButton button1,button2,button3;
     Login(){
         super("Hybrid ATM");
-
+        //image i1
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
         Image i2 = i1.getImage().getScaledInstance(100,100, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
         image.setBounds(350,10,100,100);
         add(image);
-
+        //image ii2
         ImageIcon ii1 = new ImageIcon(ClassLoader.getSystemResource("icon/card.png"));
         Image ii2 = ii1.getImage().getScaledInstance(100,100, Image.SCALE_DEFAULT);
         ImageIcon ii3 = new ImageIcon(ii2);
         JLabel iimage = new JLabel(ii3);
         iimage.setBounds(630,350,100,100);
         add(iimage);
-
+        // welcome text
         label1 = new JLabel("WELCOME TO ATM");
         label1.setForeground(Color.WHITE);
         label1.setFont(new Font("AvantGarde", Font.BOLD,38));
         label1.setBounds(230,125,450,40);
         add(label1);
-
+        //label text 2
         label2 = new JLabel("Card No:");
         label2.setFont(new Font("Ralway",Font.BOLD,28));
         label2.setForeground(Color.WHITE);
         label2.setBounds(150,190,375,30);
         add(label2);
-
+        //text field 2
         textField2 = new JTextField(15);
         textField2.setBounds(325,190,230,30);
         textField2.setFont(new Font("Arial",Font.BOLD,14));
         add(textField2);
-
+        //label text 3
         label3 = new JLabel("PIN: ");
         label3.setFont(new Font("Ralway",Font.BOLD,28));
         label3.setForeground(Color.WHITE);
         label3.setBounds(150,250,375,30);
         add(label3);
-
+        //password field 3
         passwordField3 = new JPasswordField(15);
         passwordField3.setBounds(325,250,230,30);
         passwordField3.setFont(new Font("Arial",Font.BOLD,14));
         add(passwordField3);
-
+        //button for signin
         button1 = new JButton("SIGN IN");
         button1.setFont(new Font("Arial",Font.BOLD,14));
         button1.setForeground(Color.WHITE);
@@ -63,15 +63,15 @@ public class Login extends JFrame implements ActionListener {
         button1.setBounds(300,300,100,30);
         button1.addActionListener(this);
         add(button1);
-
+        //button for clear
         button2 = new JButton("CLEAR");
         button2.setFont(new Font("Arial",Font.BOLD,14));
         button2.setForeground(Color.WHITE);
         button2.setBackground(Color.BLACK);
         button2.setBounds(430,300,100,30);
-        button2.addActionListener(this);
+        button2.addActionListener(this);//created event holder
         add(button2);
-
+        //button for signup
         button3 = new JButton("SIGN UP");
         button3.setFont(new Font("Arial",Font.BOLD,14));
         button3.setForeground(Color.WHITE);
@@ -79,14 +79,14 @@ public class Login extends JFrame implements ActionListener {
         button3.setBounds(300,350,230,30);
         button3.addActionListener(this);
         add(button3);
-
+        //image ii3
         ImageIcon iii1 = new ImageIcon(ClassLoader.getSystemResource("icon/backbg.png"));
         Image iii2 = iii1.getImage().getScaledInstance(850,480, Image.SCALE_DEFAULT);
         ImageIcon iii3 = new ImageIcon(iii2);
         JLabel iiimage = new JLabel(iii3);
         iiimage.setBounds(0,0,850,480);
         add(iiimage);
-
+        //window properties
         setLayout(null);
         setSize(850,480);
         setLocation(350,200);
@@ -99,8 +99,8 @@ public class Login extends JFrame implements ActionListener {
             if(e.getSource() == button1){
 
             }else if (e.getSource() == button2){
-                textField2.setText("");
-                passwordField3.setText("");
+                textField2.setText("");          //clear text field
+                passwordField3.setText("");      //clear password field
             }else if (e.getSource() == button3){
 
             }
